b4f3ab4ddbc53d293f851b0aea36f70ba20b1569
tugi <blueqwinn@gmail.com>
2023-02-10 21:35:28 +0300

    first commit

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
+
 # Write your code here.
+ peter_otieno =[]
+
+ take_a_number(peter_otieno, "Katy")  #=> Welcome, Katy. You are number 1  in line .
+ take_a_number(peter_otieno, "John")  #=> Welcome, John. You are number 2 in line .
+ take_a_number(peter_otieno, "Penny")  #=> Welcome, Penny. You are number  3 in line .
+
+ line(peter_otieno) #=> "This line is currently: 1.Katy 2.John 3.Penny"
+
+ now_serving(peter_otieno) #=> "Currently serving Katy."
+
+ line(peter_otieno)  #=> "This line is currently: 1.John 2.Penny"
+
+ take_a_number(peter_otieno "Ken" ) #=> Welcome, Ken. You are number 3 in line.
+
+line(peter_otieno)# => "This  line is currently: 1.John 2.Penny 3.Ken"
+
+now_serving(peter_otieno) # => "Currently serving John."
+
+line(peter_otieno) # => "The line is currently 1.penny 2.Ken"
